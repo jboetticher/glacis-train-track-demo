35c9c729df463b5cc05d717d7f0405b1859c1653
Jeremy Boetticher <me4jeremyboett@gmail.com>
2023-07-10 12:19:29 +0800

    tracks

--- src/App.js
+++ src/App.js
@@ -1,24 +1,101 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
+import React, { useState } from 'react';
+import Web3 from 'web3';
+import detectEthereumProvider from '@metamask/detect-provider';
+import { Container, Row, Col, Dropdown, Button, Image } from 'react-bootstrap';
+
+const rightImg = './track-right.png';
+const leftImg = './track-left.png';
+
+// Add your contract address here
+const contractAddress = 'your_contract_address';
+
+// And your contract ABI
+const contractABI = [];
 
 function App() {
+  const [account, setAccount] = useState(null);
+  const [image, setImage] = useState(rightImg);
+  const [web3, setWeb3] = useState(null);
+  const [contract, setContract] = useState(null);
+
+  const connectMetamask = async () => {
+    const provider = await detectEthereumProvider();
+
+    if (provider) {
+      const web3Instance = new Web3(window.ethereum);
+      const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+      const contractInstance = new web3Instance.eth.Contract(contractABI, contractAddress);
+
+      setWeb3(web3Instance);
+      setAccount(accounts[0]);
+      setContract(contractInstance);
+    } else {
+      console.log('Please install MetaMask!');
+    }
+  };
+
+  const toggleImage = () => {
+    setImage(prevImage => prevImage === rightImg ? leftImg : rightImg);
+  };
+
+  // This is your function to send a transaction
+  const sendTransaction = async () => {
+    if (!web3 || !contract) {
+      console.log('Web3 or contract not loaded');
+      return;
+    }
+
+    // Replace 'myFunction' with your contract's function
+    // Replace 'args1', 'args2' with your actual arguments
+    const gasPrice = await web3.eth.getGasPrice();
+    const gasEstimate = await contract.methods.myFunction('args1', 'args2').estimateGas({ from: account });
+
+    contract.methods.myFunction('args1', 'args2')
+      .send({ from: account, gasPrice: gasPrice, gas: gasEstimate })
+      .on('transactionHash', (hash) => {
+        console.log('Transaction sent with hash: ', hash);
+      })
+      .on('error', (error) => {
+        console.log('Error sending transaction: ', error);
+      });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Row className="align-items-center">
+        <Col><h1>Glacis Train Track Demo</h1></Col>
+        <Col>
+          <Dropdown>
+            <Dropdown.Toggle variant="success" id="dropdown-basic">
+              Dropdown 1
+            </Dropdown.Toggle>
+            <Dropdown.Menu>
+              <Dropdown.Item href="#/action-1">Fantom</Dropdown.Item>
+              <Dropdown.Item href="#/action-2">Fuji</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+        </Col>
+        <Col>
+          <Dropdown>
+            <Dropdown.Toggle variant="success" id="dropdown-basic">
+              Dropdown 2
+            </Dropdown.Toggle>
+            <Dropdown.Menu>
+              <Dropdown.Item href="#/action-1">Fantom</Dropdown.Item>
+              <Dropdown.Item href="#/action-2">Fuji</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+        </Col>
+        <Col>
+          <Button onClick={connectMetamask}>Connect Metamask</Button>
+          <Button onClick={sendTransaction}>Send Transaction</Button>
+        </Col>
+      </Row>
+      <Row className="justify-content-center">
+        <Image onClick={toggleImage} src={image} rounded />
+      </Row>
+    </Container>
   );
 }
 
